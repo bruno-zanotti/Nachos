0d547003669c02e541c729dad08be77d178c8f7a
Bruno <brunozanotti96@gmail.com>
2020-06-04 21:07:51 -0300

    Plancha 1 completa!

--- code/threads/thread_test.cc
+++ code/threads/thread_test.cc
@@ -16,6 +16,10 @@
 #include <stdio.h>
 #include <string.h>
 
+/// PLANCHA 1 - Ejercicio 15
+#ifdef SEMAPHORE_TEST
+    Semaphore *sem = new Semaphore("semaphore test", 3);
+#endif
 
 /// Loop 10 times, yielding the CPU to another ready thread each iteration.
 ///
@@ -30,26 +34,45 @@ SimpleThread(void *name_)
     // If the lines dealing with interrupts are commented, the code will
     // behave incorrectly, because printf execution may cause race
     // conditions.
+
+    /// PLANCHA 1 - Ejercicio 15-16
+    #ifdef SEMAPHORE_TEST
+        DEBUG('s', "Thread: %s tries to make P()\n", name);
+        sem->P();
+    #endif
+
     for (unsigned num = 0; num < 10; num++) {
         printf("*** Thread `%s` is running: iteration %u\n", name, num);
         currentThread->Yield();
     }
     printf("!!! Thread `%s` has finished\n", name);
+
+    /// PLANCHA 1 - Ejercicio 15-16
+    #ifdef SEMAPHORE_TEST
+        DEBUG('s', "Thread: %s tries to make V()\n", name);
+        sem->V();
+    #endif
+
 }
 
 /// Set up a ping-pong between several threads.
 ///
 /// Do it by launching ten threads which call `SimpleThread`, and finally
 /// calling `SimpleThread` ourselves.
+
+/// PLANCHA 1 - Ejercicio 14
 void
 ThreadTest()
 {
     DEBUG('t', "Entering thread test\n");
+    DEBUG('s', "Entering semaphore test\n");
 
-    char *name = new char [64];
-    strncpy(name, "2nd", 64);
-    Thread *newThread = new Thread(name);
-    newThread->Fork(SimpleThread, (void *) name);
+    for (char num = '2'; num < '6'; num++) {
+        char *name = new char [1];
+        strncpy(name, &num, 1);
+        Thread *newThread = new Thread(name);
+        newThread->Fork(SimpleThread, (void *) name);
+    }
 
-    SimpleThread((void *) "1st");
+    SimpleThread((void *) "1");
 }
